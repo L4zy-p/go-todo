55b944baf674ae2d630d604199d830e0e73bbb72
L4zy-p <hemhongsa.p94@gmail.com>
2022-02-03 12:41:04 +0700

    [controller]: getTodoListById getTodoListByUsername deleteTodoListById upload

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
 	"go-todo/models"
+	"io"
+	"log"
+	"os"
 
 	"net/http"
 
@@ -40,5 +44,91 @@ func CreateTodoList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"staus": "ok",
 		"data":  todoList,
+	})
+}
+
+func GetTodoListById(c *gin.Context) {
+	var todoList models.Todo
+
+	// ให้ err เท่ากับการค้นหา todo จาก id ที่ param id มา แล้วเช็ค err ต่อว่า มี error หรือป่าว
+	// First เป็นการหาตัวแรก ที่มี id นี้แค่ตัวเดียวเท่านั้น
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&todoList).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Record not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": todoList,
+	})
+}
+
+func GetTodoListByUsername(c *gin.Context) {
+	var todoList []models.Todo
+
+	// ให้ err เท่ากับการค้นหา todo จาก username ที่ query username มา แล้วเช็ค err ต่อว่า มี error หรือป่าว
+	// Find จะเป็นการหาทุกตัวที่มี Username นี้
+	if err := models.DB.Where("username = ?", c.Query("username")).Find(&todoList).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Record not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": todoList,
+	})
+}
+
+func DeleteTodoListById(c *gin.Context) {
+	var todoList models.Todo
+
+	// ให้ err เท่ากับการค้นหา todo จาก id ที่ param id มา แล้วเช็ค err ต่อว่า มี error หรือป่าว
+	// First เป็นการหาตัวแรก ที่มี id นี้เพื่อที่จะทำการลบต่อ
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&todoList).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Record not found",
+		})
+		return
+	}
+
+	models.DB.Delete(&todoList)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Delete successfuly",
+	})
+}
+
+func Upload(c *gin.Context) {
+	// ดู field file จาก request formfile ที่ส่งมา ซึ่ง return ค่ามาให้ 3 ตัวคือ file, header, err
+	file, header, err := c.Request.FormFile("file")
+
+	// เช็คมาว่าที่ส่ง formfile มามี error หรือป่าว
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("file err %s", err.Error()))
+		return
+	}
+
+	// get ชื่อไฟล์จาก header
+	filename := header.Filename
+
+	// ทำการสร้างไฟล์ให้อยู่ในโฟลเดอร์ public
+	out, err := os.Create("public/" + filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer out.Close()
+
+	// ทำการ copy file ที่ส่งมาจาก request แล้วใส่ไปใน path ที่เกิดจาก os.Create
+	_, err = io.Copy(out, file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filepath := "http://localhost:3001/file/" + filename
+	c.JSON(http.StatusOK, gin.H{
+		"filepath": filepath,
 	})
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go-todo/controllers"
 	"go-todo/models"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,7 +15,15 @@ func main() {
 
 	r.GET("/", controllers.CheckStatus)
 	r.GET("/todo", controllers.GetAllLists)
+	r.GET("/todo/:id", controllers.GetTodoListById)
 	r.POST("/todo", controllers.CreateTodoList)
+	r.POST("/upload", controllers.Upload)
+	r.DELETE("/todo/:id", controllers.DeleteTodoListById)
+
+	r.GET("/user", controllers.GetTodoListByUsername)
+
+	// ให้ path file แสดง file ทั้งหมดที่อยู่ใน public
+	r.StaticFS("/file", http.Dir("public"))
 
 	r.Run(":3001")
 }
